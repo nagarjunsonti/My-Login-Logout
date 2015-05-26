8c85f6e89f0e80b0aff62c2fe72ae2d8d244f734
nagarjun <nagarjunsonti@gmail.com>
2015-05-26 12:01:19 +0530

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,17 +1,18 @@
-$(document).ready(function(){
+var myloginlogout = jQuery.noConflict();
+myloginlogout(document).ready(function(){
 
-$("#show_custom_login_button").click(function(){
-	$( "#login-dropdown" ).hide(1000);	
-	$( "#custom_loginid" ).show(1000);
-	$( "#show_custom_login_button" ).hide(1000);
+myloginlogout("#show_custom_login_button").click(function(){
+	myloginlogout( "#login-dropdown" ).hide(1000);	
+	myloginlogout( "#custom_loginid" ).show(1000);
+	myloginlogout( "#show_custom_login_button" ).hide(1000);
 
 });
 
-$("#hide_custom_login_button").click(function(){
-	$( "#custom_loginid" ).hide( 1000);
-	$( "#custom_login_url" ).val("");
-	$( "#login-dropdown" ).show(1000);
-	$( "#show_custom_login_button" ).show(1000);//button
+myloginlogout("#hide_custom_login_button").click(function(){
+	myloginlogout( "#custom_loginid" ).hide( 1000);
+	myloginlogout( "#custom_login_url" ).val("");
+	myloginlogout( "#login-dropdown" ).show(1000);
+	myloginlogout( "#show_custom_login_button" ).show(1000);//button
 
 });
 });
